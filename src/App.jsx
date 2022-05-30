4b6112927f85ef719f238c2daefb358cec797b11
Paulina Flores <modsmith2@gmail.com>
2022-05-29 20:17:05 -0500

    quotes

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import { useState } from 'react'
+import './App.css'
+import QuoteBox from './components/QuoteBox'
+import quotes from './json/quotes.json'
+
+const colors = [
+  '#6B12A6', '#F279DE', '#CCBBF2', '#F9C903', '#F2C4E2',
+  '#28255A', '#F26BC3'
+]
+
+
+function App() {
+
+  const createNumberRandom = array => {
+    return Math.floor(Math.random() * array.length)
+  }
+
+  const getElementsRandom = array=>{
+    const i= createNumberRandom(array)
+    return array[i]
+  }
+ 
+  const [quoteRandom, setquoteRandom] = useState(getElementsRandom(quotes))
+  const [colorRandom, setcolorRandom] = useState(getElementsRandom(colors))
+
+  const clickButton= ()=>{
+    setquoteRandom(getElementsRandom(quotes))
+    setcolorRandom (getElementsRandom(colors))
+  }
+
+
+  const objectStyle = {
+    backgroundColor: colorRandom
+  }
+
+  return (
+    <div style={objectStyle} className='App'>
+
+      <QuoteBox
+        quoteRandom={quoteRandom}
+        colorRandom={colorRandom}
+        clickButton={clickButton}
+      />
+
+
+    </div>
+
+  )
+}
+
+export default App
---
+++ src/components/QuoteBox.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const QuoteBox = ({ quoteRandom, colorRandom,clickButton }) => {
+
+  const styleColor = { backgroundColor: colorRandom }
+
+  return (
+    <article className='box-quote' style={{ color: colorRandom }}>
+      <p className='box-quote__phrase'><span>"</span>{quoteRandom.quote}</p>
+      <span className='box-quote__autor'>{quoteRandom.author}.</span>
+      <button
+       style={styleColor} 
+       className='box-quote__btn' 
+       onClick={clickButton}
+       >&#62;</button>
+    </article>
+  )
+}
+
+export default QuoteBox
